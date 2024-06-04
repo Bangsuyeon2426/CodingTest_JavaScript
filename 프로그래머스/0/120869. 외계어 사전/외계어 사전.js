f8ecfc1738a2cc63b4e94d2c3470dae8342e6885
Bang suyeon <127937179+Bangsuyeon2426@users.noreply.github.com>
2024-06-04 21:33:43 +0900

    [level 0] Title: 외계어 사전, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.js
@@ -0,0 +1,9 @@
+function solution(spell, dic) {
+    const spellSorted = spell.sort().join('');
+    for (let word of dic) {
+        if (word.split('').sort().join('') === spellSorted) {
+            return 1;
+        }
+    }
+    return 2;
+}
